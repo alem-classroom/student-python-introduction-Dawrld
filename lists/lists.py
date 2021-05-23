224fea65707575814f1d543fd3f4896fc1c1b99e
Dawrld <52791111+Dawrld@users.noreply.github.com>
2021-05-24 01:19:06 +0600

    Update lists.py

--- lists/lists.py
+++ lists/lists.py
@@ -1,33 +1,25 @@
 def size_of_list(list):
     return len(list)
-    # return size of list
 
 def add_elem_to_list(list, elem):
     list.append(elem)
     return list
-    # add elem to list and return the list
 
 def delete_elem_from_list(list, index = -1):
-    if -len(list)<=index<len(list):
-        return list.pop(index)
+    if len(list) >index:
+        list.pop(index)
+        return list
     else:
         return []
-        
-    
-    
-    # delete element from list, such that its index is index
-    # if index is invalid, return empty list
 
 def count_elements_in_list(list, x):
     return list.count(x)
-    # count elements in the list that are equal to x and return the count
 
 def sort_list(list):
     list.sort()
     return list
-    # return sorted list
 
 def reverse(list):
     list.reverse()
     return list
-    # return reversed list
+
